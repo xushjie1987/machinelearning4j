2233da5dd372ce69a6eaa52bb57df69113486bad
shengjie xu <xsj1987@foxmail.com>
2017-08-13 00:43:45 +0800

    clustering, KMeans

--- src/main/java/tutorials/clustering/TutorialKMeans.java
+++ src/main/java/tutorials/clustering/TutorialKMeans.java
@@ -32,31 +32,31 @@ import net.sf.javaml.tools.data.FileHandler;
 /**
  * This tutorial shows how to use a clustering algorithm to cluster a data set.
  * 
- * 
  * @author Thomas Abeel
- * 
  */
 public class TutorialKMeans {
-
+    
     /**
      * Tests the k-means algorithm with default parameter settings.
      */
     public static void main(String[] args) throws Exception {
-
+        
         /* Load a dataset */
-        Dataset data = FileHandler.loadDataset(new File("devtools/data/iris.data"), 4, ",");
+        Dataset data = FileHandler.loadDataset(new File("devtools/data/iris.data"),
+                                               4,
+                                               ",");
         /*
-         * Create a new instance of the KMeans algorithm, with no options
-         * specified. By default this will generate 4 clusters.
+         * Create a new instance of the KMeans algorithm, with no options specified. By default this
+         * will generate 4 clusters.
          */
         Clusterer km = new KMeans();
         /*
-         * Cluster the data, it will be returned as an array of data sets, with
-         * each dataset representing a cluster
+         * Cluster the data, it will be returned as an array of data sets, with each dataset
+         * representing a cluster
          */
         Dataset[] clusters = km.cluster(data);
         System.out.println("Cluster count: " + clusters.length);
-
+        
     }
-
+    
 }
